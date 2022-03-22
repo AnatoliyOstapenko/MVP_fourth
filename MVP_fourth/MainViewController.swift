12a0ccb0b3ff1799eb116793a9b5c3e55aaa1424
Anatolii Ostapenko <anatoliy.ostapenko@gmail.com>
2022-03-22 16:46:41 +0200

    Add screen programmatically

---
+++ MVP_fourth/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  MVP_fourth
+//
+//  Created by AnatoliiOstapenko on 22.03.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .cyan
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
